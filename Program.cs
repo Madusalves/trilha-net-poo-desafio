c8d41e1def76b2e100caae090f7e055e412c3411
Maria Eduarda <malvesduarte444@gmail.com>
2023-12-10 21:15:26 -0300

    commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia1 = new Nokia("Nokia 1", 512, 1.3, 5);
+Iphone iphone1 = new Iphone("Iphone 1", 256, 2.0, 7);
